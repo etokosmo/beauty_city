4afd958d379f019a0418f720b2c1076f76c440df
MaksAnikeev <anikeev.maks@rambler.ru>
2022-12-14 22:43:04 +0300

    fulling BD

--- service/admin.py
+++ service/admin.py
@@ -1,4 +1,5 @@
 from django.contrib import admin
+from django.utils.html import format_html
 
 from .models import ServiceCategory, Salon, Service, Master, User, Timeslot
 
@@ -20,12 +21,37 @@ class ServiceAdmin(admin.ModelAdmin):
 
 @admin.register(Master)
 class MasterAdmin(admin.ModelAdmin):
-    pass
+    list_display = [
+        'first_name',
+        'second_name',
+        'get_image_preview',
+    ]
+    readonly_fields = [
+        'get_image_preview',
+    ]
+    def get_image_preview(self, obj):
+        if not obj.image:
+            return 'нет картинки'
+        return format_html('<img src="{url}" style="max-height: 100px;"/>',
+                           url=obj.image.url)
 
 
 @admin.register(User)
 class UserAdmin(admin.ModelAdmin):
-    pass
+    list_display = [
+        'first_name',
+        'second_name',
+        'get_image_preview',
+    ]
+
+    readonly_fields = [
+        'get_image_preview',
+    ]
+    def get_image_preview(self, obj):
+        if not obj.image:
+            return 'нет картинки'
+        return format_html('<img src="{url}" style="max-height: 100px;"/>',
+                           url=obj.image.url)
 
 
 @admin.register(Timeslot)
