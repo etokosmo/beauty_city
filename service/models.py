c5f3ad9ced88d6255dd92e012a499dbce230696a
leeny_the_bear <leeny@inbox.ru>
2022-12-14 15:55:33 +0600

    init models

--- service/models.py
+++ service/models.py
@@ -1,3 +1,31 @@
 from django.db import models
 
-# Create your models here.
+
+class ServiceCategory(models.Model):
+    title = models.CharField(verbose_name="Название категории", max_length=200)
+
+
+class Salon(models.Model):
+    title = models.CharField(verbose_name="Название салона", max_length=200)
+    address = models.CharField(verbose_name="Адрес салона", max_length=200)
+
+
+class Service(models.Model):
+    title = models.CharField(verbose_name="Название услуги", max_length=200)
+    price = models.PositiveIntegerField(verbose_name="Стоимость услуги")
+    category = models.ForeignKey(
+        ServiceCategory,
+        on_delete=models.CASCADE,
+        verbose_name="Категория услуги",
+    )
+
+
+class Master(models.Model):
+    first_name = models.CharField(verbose_name="Имя мастера", max_length=200)
+    second_name = models.CharField(
+        verbose_name="Фамилия мастера", max_length=200
+    )
+    service = models.ManyToManyField(
+        Service, verbose_name="Услуги мастера", related_name="masters"
+    )
+    salon = models.ForeignKey(Salon, on_delete=models.SET_NULL, null=True)
