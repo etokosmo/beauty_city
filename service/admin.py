84a0eead44389d1a1a357a0db0a9a4b70c90aab4
Denis Polyakov <p-ov.da@ya.ru>
2022-12-14 18:00:07 +0300

    Add admin panel

--- service/admin.py
+++ service/admin.py
@@ -1,3 +1,33 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import ServiceCategory, Salon, Service, Master, User, Timeslot
+
+
+@admin.register(ServiceCategory)
+class ServiceCategoryAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(Salon)
+class SalonAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(Service)
+class ServiceAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(Master)
+class MasterAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(User)
+class UserAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(Timeslot)
+class TimeslotAdmin(admin.ModelAdmin):
+    pass
